97ca7c1754aecb188d1cf41a6686827902fa32d5
azamatshauenbekov <bravlstarstop05@mail.ru>
2023-12-09 23:16:50 +0600

    Deploy

--- Weeek77-master/src/main/java/com/example/Weeek7/service/EmailService.java
+++ Weeek77-master/src/main/java/com/example/Weeek7/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.example.Weeek7.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,12 @@ public class EmailService {
 
     private final JavaMailSender javaMailSender;
 
+    @Value("${gmail}")
+    private String mail;
+
     public void sendEmail(String toEmail, String activationLink) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(mail);
         message.setTo(toEmail);
         message.setSubject("Активируйте свой аккаунт");
         message.setText("Для активации Вашего аккаунта, пройдите по ссылке: \n\n" + activationLink);
